b768f78f8c85da2931a06468ed9348b2491b7737
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2026-02-14 18:02:48 -0800

    fix config validation

--- generate-supervisord-config.sh
+++ generate-supervisord-config.sh
@@ -15,9 +15,22 @@ if [ -z "${NUMPROCS}" ]; then
   exit 1
 fi
 
+GENERATOR_MODE=${GENERATOR_MODE:-"metrics"}
+
+if [ "${GENERATOR_MODE}" = "all" ] && [ -z "${LOADGEN_DOCKER_IMAGE}" ]; then
+  echo "expected LOADGEN_DOCKER_IMAGE when GENERATOR_MODE=all"
+  exit 1
+fi
+
+if [ "${GENERATOR_MODE}" = "all" ]; then
+  OTELCOL_COMMAND="./run-collector.sh docker run --rm -e ITERATION=%(ENV_ITERATION)s -e INSTANCE=%(process_num)d -v %(here)s/config.yaml:/config.yaml:ro ${LOADGEN_DOCKER_IMAGE} --config /config.yaml"
+else
+  OTELCOL_COMMAND="./run-collector.sh /usr/bin/otelcol-contrib --config config.yaml"
+fi
+
 cat > supervisord.ini << EOF
 [program:otelcol]
-command=./run-collector.sh /usr/bin/otelcol-contrib --config config.yaml
+command=${OTELCOL_COMMAND}
 environment=INSTANCE=%(process_num)d,DURATION=${DURATION},DELAY_START=${DELAY_START}
 numprocs=${NUMPROCS}
 process_name=%(program_name)s-%(process_num)d
